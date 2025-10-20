730bfccb0b4444e113c0d630e5e710a09f16c24c
TABAKO <76990062+Sata-P@users.noreply.github.com>
2025-10-20 19:25:04 +0700

    edit comment multi

--- assembly_lang/multi.asm
+++ assembly_lang/multi.asm
@@ -3,18 +3,18 @@
         lw      0   3   mplier  //load ค่าจาก mem -> reg[3] = mplier    
         lw      0   4   bitmas  //load ค่าจาก mem -> reg[4] = bitmask    
         lw      0   5   bitc    //load ค่าจาก mem -> reg[4] = bitcount   
-        lw      0   6   neg1    neg1 //load ค่าจาก mem -> reg[6] = -1   
+        lw      0   6   neg1    //load ค่าจาก mem -> reg[6] = -1   
 
-loop    nand    3   4   7        [ // สองบรรทัดนี้ใช้ดูว่าbitตัวแรกเป็น 1 หรือ 0  
-        nand    7   7   7        ] 
+loop    nand    3   4   7        // สองบรรทัดนี้ใช้ดูว่าbitตัวแรกเป็น 1 หรือ 0 
+        nand    7   7   7        
         beq     0   7   zero     // ถ้า current bit = 0 ให้โดดไปที่บรรทัด zero 
         add     1   2   1        // แต่ถ้าไม่ให้ทำการ(ซึ่งแปลว่ามันคือ 1) บวกตัวตั้ง(mcand)เข้ากับผลลัพธ์ result=result+mcand 
 
 zero    add     2   2   2        // mcand left shift 1 bit  
-        add     4   4   4        // bitnask left shift 1 bit
+        add     4   4   4        // bitmask left shift 1 bit
         add     6   5   5        // เอาลบหนึ่งลดจำนวน loop ลง 
-        beq     5   0   end      [ // ถ้า จนloop=0 จบการทำงาน   
-        beq     0   0   loop      //ถ้าไม่ให้ไปทำ loop อีกรอบ ]  
+        beq     5   0   end      // ถ้าจำนวน loop=0 จบการทำงาน     
+        beq     0   0   loop      //ถ้าไม่ให้ไปทำ loop อีกรอบ  
         noop                    
 
 end     halt                        
