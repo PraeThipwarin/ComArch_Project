8cfe973a3c89c666a219304a13cc14499507dcf8
TABAKO <76990062+Sata-P@users.noreply.github.com>
2025-10-20 19:57:16 +0700

    edit comment division

--- assembly_lang/divide.asm
+++ assembly_lang/divide.asm
@@ -1,4 +1,4 @@
-	lw      0 4 pos1        //load reg[4] = +1
+		lw      0 4 pos1        //load reg[4] = +1
         lw      0 1 dvdnd       //load reg[1] = dividend
         lw      0 2 dvsor       //load reg[2] = divisor
         lw      0 6 sgnmsk      //load reg[6] = signmask
@@ -8,17 +8,17 @@
 
 div     sw      0 7 rtadr       //เอา reg[07] -> mem rtadr
         add     0 0 3           // กำหนดค่าเริ่มต้นให้ผลหาร
-        nand    2 2 5           [// เอาตัวหารมาทำเป็นลบโดยใช้ 2's complement
-        add     5 4 5           ]
+        nand    2 2 5           // เอาตัวหารมาทำเป็นลบโดยใช้ 2's complement
+        add     5 4 5           
 
 loop    add     1 5 1           // เอาตัวตั้ง - ตัวหาร
         add     3 4 3           // quotient = quotient + 1
-        nand    1 6 7           // 
-        nand    7 7 7
-        beq     7 0 cont        // เอาไว้ดักเช็คว่า dvdnd - dvsor ตัว dvdnd มันมีค่าเป็น +,- เผื่อดูว่ามันน้อยกว่ายังหรือมากกว่ายัง
-        add     1 2 1           // คืนค่าตัวตั้งกลับคืนมา (dvdnd = dvdnd + dvsor)
+        nand    1 6 7           // ดูว่า MSB ของ ตัวตั้ง ว่าเป็น 1 หรือ 0 
+        nand    7 7 7           //     
+        beq     7 0 cont        // เอาไว้ดักเช็คว่า dvdnd - dvsor ตัว dvdnd มันมีค่าเป็น +,- เพื่อดูว่ามันน้อยกว่ายังหรือมากกว่ายัง
+        add     1 2 1           // คืนค่าตัวตั้งกลับคืนมา (dvdnd = dvdnd + dvsor) เพราะว่าเราวนลูปเกินไป 1 รอบเลยต้องบวกคืน
         nand    0 0 7           // reg[7] = -1
-        add     3 7 3           // ลบค่า quotient ที่เกินมา 1 
+        add     3 7 3           // ลบค่า quotient ที่เกินมา 1 ที่เกินมาจากการทำลูปรอบสุดท้าย
         beq     0 0 done        // กระโดดไป label จบ
 
 cont    beq     0 0 loop        // ทำ loop ต่อ
@@ -30,5 +30,5 @@ dvdnd   .fill   13
 dvsor   .fill   4
 pos1    .fill   1
 dvadr   .fill   div
-sgnmsk  .fill   -2147483648
+sgnmsk  .fill   -2147483648  //ถ้าเก็บค่าได้มากสุด 16 bits ให้ใช้ค่า -65536 เพราะว่า -2^16 เป็น MSB
 rtadr   .fill   0
